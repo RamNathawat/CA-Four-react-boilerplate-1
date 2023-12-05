27da22d8735ba0243d2c3e4f7502b61bdfa978a4
aniketgoyal51 <aniketgoyal791@gmail.com>
2023-12-05 10:11:46 +0530

    CA-4

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React , {useContext} from 'react'
+import { marks } from './QuestionBox.js'
 
 export default function Result() {
+
+  const score=useContext(marks)
+
+  function restart(){
+    <>
+      {window.location.reload(false)}
+    </>
+  }
+
   return (
     <div>
-      
+      <h1>Final Score</h1>
+      <h2>{score} out of 5</h2>
+      <h2>({(score/5)*100}%)</h2>
+      <button onClick={restart} className='restart'>Restart</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <div>
+  <>
     <App />
-  </div>,
+  </>,
   document.getElementById('root')
 );
